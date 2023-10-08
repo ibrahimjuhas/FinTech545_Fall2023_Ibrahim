bdf633e3b14faebc80e2d97d21fe52b6d35d2416
ibrahimjuhas <Ahmed Ibrahim>
2023-10-08 07:41:11 -0400

    adding week05 answers

--- week05_answers/var_allcalc.py
+++ week05_answers/var_allcalc.py
@@ -80,29 +80,3 @@ def var_historic(returns, confidence_level=0.95):
     data_sorted = returns.sort_values()
     var_hist = -data_sorted.quantile(1 - confidence_level)
     return var_hist
-
-
-# 6. Calculate the returns
-def return_calculate(prices, method="DISCRETE", date_column="Date"):
-    vars = prices.columns
-    n_vars = len(vars)
-    vars = [v for v in vars if v != date_column]
-    if n_vars == len(vars):
-        raise ValueError(f"{date_column} not in DataFrame {vars}")
-    n_vars = len(vars)
-    p = prices[vars].to_numpy()
-    n, m = p.shape
-    p2 = np.empty((n - 1, m))
-    for i in range(n - 1):
-        p2[i, :] = p[i + 1, :] / p[i, :]
-    if method.upper() == "DISCRETE":
-        p2 -= 1.0
-    elif method.upper() == "LOG":
-        p2 = np.log(p2)
-    else:
-        raise ValueError(f'method {method} must be in ("LOG", "DISCRETE")')
-    dates = prices.iloc[1:, prices.columns.get_loc(date_column)]
-    out = pd.DataFrame({date_column: dates})
-    for i in range(n_vars):
-        out[vars[i]] = p2[:, i]
-    return out
